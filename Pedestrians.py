d9c94cbf109636ca2d3a464e92a41b7821fa19c9
Otho Komatsu <otho.tk@hotmail.com>
2018-06-17 14:20:15 -0300

    Modificação no programa
    Falta: skin detector e hough transform

--- Pedestrians.py
+++ Pedestrians.py
@@ -29,8 +29,8 @@ def multi_frame_differecing(Frames_five):
 	ret, D[2] = cv.threshold(D[2],Threshold,255,cv.THRESH_BINARY)
 	ret, D[3] = cv.threshold(D[3],Threshold,255,cv.THRESH_BINARY)
 
-	LAO1 = D[1,:,:]*D[2,:,:]
-	LAO2 = D[0,:,:]*D[3,:,:]
+	LAO1 = cv.bitwise_and(D[1,:,:],D[2,:,:])
+	LAO2 = cv.bitwise_and(D[0,:,:],D[3,:,:])
 
 	MR = np.zeros((height,width), np.uint8)
 
@@ -39,10 +39,8 @@ def multi_frame_differecing(Frames_five):
 	return MR
 
 
-
-
 def main():
-	cap = cv.VideoCapture("./Videos/MVI_0118.MOV")
+	cap = cv.VideoCapture("./Videos/terrace1-c0.avi")
 	Frames_five = []
 	fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
 
@@ -57,7 +55,6 @@ def main():
 		gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 		height,width = gray.shape
 
-	
 		# Get each frame
 		Frames_five.append(gray)
 
@@ -65,60 +62,50 @@ def main():
 		if(len(Frames_five)%5 == 0):
 
 			frame_number = len(Frames_five)/5 
+
 			MR = multi_frame_differecing(Frames_five)
-			fgmask = fgbg.apply(Frames_five[frame_number-3])
 
-			MR += fgmask
+			fgmask = fgbg.apply(frame)
 
-			MR = imutils.resize(MR, width=min(400, MR.shape[1]))
-		
-			'''
-			# Test images MR
-			path =  "./MR_images/"+ "MR_" + str(len(Frames_five)/5) + ".png"
+			fgmask = imutils.resize(fgmask, width=min(400, fgmask.shape[1]))
 
-
-			cv.imwrite(path, MR)
-
-			if (len(Frames_five) == 800):
-				break
 			'''
 
-		fgmask = fgbg.apply(gray)
+			# Opening operation in noising videos
 
-		kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(5,5))
+			kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
 
-		fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+			fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+			'''
 
-		frame = imutils.resize(frame, width=min(400, frame.shape[1]))
+			frame = imutils.resize(frame, width=min(400, frame.shape[1]))
 
-		fgmask = imutils.resize(fgmask, width=min(400, fgmask.shape[1]))
+			fgmask = imutils.resize(fgmask, width=min(400, fgmask.shape[1]))
 
-		hog = cv.HOGDescriptor()
+			# Histogram of oriented gradients
+			hog = cv.HOGDescriptor()
 
-		hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+			hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
 
-		(rects, weights) = hog.detectMultiScale(fgmask, winStride=(4, 4), \
-		 padding=(8, 8), scale=1.05)
+			# Find human shape
 
+			(rects, weights) = hog.detectMultiScale(fgmask, winStride=(4, 4), \
+			 padding=(2, 2), scale=1.05)
 
-		'''
-		for (x, y, w, h) in rects:
-			cv.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+			rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
 
-		'''
+			# Non-max-supression process
 
-		rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-		pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+			pick = non_max_suppression(rects, probs=None, overlapThresh=0.4)
+			for (xA, yA, xB, yB) in pick:
+				cv.rectangle(frame, (xA, yA), (xB, yB), (255, 0, 100), 2)
 
-		for (xA, yA, xB, yB) in pick:
-			cv.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+			cv.imshow('frame',frame)	
 
 		# Display the resulting frame
-		cv.imshow('frame',frame)
 		if cv.waitKey(1) & 0xFF == ord('q'):
 			break
 
-	# When everything done, release the capture
 	cap.release()
 	cv.destroyAllWindows()
 
